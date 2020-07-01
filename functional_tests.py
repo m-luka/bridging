2b21b5246acbb00c9b2fa763671b8121ced9f058
m-luka <luk4mat@gmail.com>
2020-07-01 17:46:46 +0200

    Added first functional test

---
+++ functional_tests.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_cv_page(self):
+        # Check cv page
+        self.browser.get('http://localhost:8000')
+
+        # Check title
+        self.assertIn('Bridging coursework', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('My blog', header_text)
+
+
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')
--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
