57063fd836b4b79380d77433bb0eea009b837d50
m-luka <luk4mat@gmail.com>
2020-07-01 18:31:56 +0200

    Added more test following TDD

--- blog/tests.py
+++ blog/tests.py
@@ -11,3 +11,8 @@ class CVPageTest(TestCase):
     def test_uses_cv_home_template(self):
         response = self.client.get('/cv')
         self.assertTemplateUsed(response, 'blog/base.html', 'blog/cv.html')
+    
+    def test_can_save_a_POST_request(self):
+	    response = self.client.post('/cv', data={'item_text': 'A new list item'})
+	    self.assertIn('A new list item', response.content.decode())
+	        
--- functional_tests.py
+++ functional_tests.py
@@ -21,12 +21,27 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('My blog', header_text)
         
         # Check form
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a CV item'
+        )
 
         # Check input
+        inputbox.send_keys('Education')
         
         # Check the page updates and the page lists the item
+        inputbox.send_keys(Keys.ENTER)  
+        time.sleep(1)  
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  
+        self.assertTrue(
+            any(row.text == '1: Education' for row in rows)
+        )
         
         # Text box still there, another CV item input
+        self.fail('Finish the test!')
 		
 		# The page updates again, and now shows both items
 		
